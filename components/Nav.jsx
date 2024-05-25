c2b1af6806e424c7df0c5beafe6e4539b72be2a2
Ishant garg <ishantgarg2702014@gmail.com>
2024-05-25 15:28:28 +0530

    create campaign added

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,6 +4,7 @@
 //component imports
 import Link from "next/link"
 import Image from "next/image"
+import { Button  } from 'antd';
 
 //asset imports
 import logo from "../public/assets/images/logo.svg"
@@ -157,8 +158,15 @@ const Nav = () => {
             }
           </>
         } */}
-
+ 
       </div>
+
+      <Link href={'/campaign/new'}>
+       <button
+    class="middle none center mr-3 rounded-lg bg-primary-orange py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+    data-ripple-light="true"
+  >Create Campaign
+  </button></Link>
     </nav>
   )
 }
