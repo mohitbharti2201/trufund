2de6d31d349912a71aa17a48387e054c9ce99d2f
Aryan <aryan03pandey@gmail.com>
2024-05-25 10:38:24 +0530

    Initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import "@/styles/globals.css"
+// import Provider from '@components/Provider'
+import Nav from "@/components/Nav"
+
+export const metadata = {
+    title: 'TruFund',
+    description: 'Fund the true cause'
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html>
+        <body>
+            {/* <Provider> */}
+                <div className='main'>
+                    <div className='gradient' />
+                </div>            
+
+                <main className='app'>
+                    <Nav />
+                    {children}
+                </main>
+            {/* </Provider> */}
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
